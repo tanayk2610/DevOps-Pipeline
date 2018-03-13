1681289e2bebca6aa9105c4f3b52d8af98ea788e
Pushpendra Singh Patel <ppatel16@ncsu.edu>
2018-03-13 22:28:38 +0000

    merge fixed

--- roles/fuzzer/templates/fuzzer.js
+++ roles/fuzzer/templates/fuzzer.js
@@ -1,24 +1,32 @@
 var Random = require('random-js');
 var fs = require('fs');
-var glob = require('glob');
 
 const execSync = require('child_process').execSync;
 
-var getJavaFiles = function (path) {
+const JENKINS_USER = process.env.JENKINS_USER,
+      JENKINS_PASSWORD = process.env.JENKINS_PASSWORD;
+
+var getJavaFiles = function () {
     var javaFiles = [];
-    var files =  glob.sync(path + "/**/*.java");
-    console.log(files.toString());
-    console.log(path);
-    files.forEach(function(file){
-            javaFiles.push(file);
+    
+    var files = execSync("ls /iTrust2-v2/iTrust2/src/main/java/edu/ncsu/csc/itrust2/**/**/*.java")
+
+    // console.log("files are: "+ files.toString());
+
+    var javafiles = files.toString().trim().split("\n");
+    // console.log("number of files found"+files.length);
+
+    javafiles.forEach(function(file){
+        javaFiles.push(file);
     })
-    console.log(javaFiles.toString());
+
+    // console.log(javaFiles.toString());
     return javaFiles;
 }
 
 var fuzzer = {
     random : new Random(Random.engines.mt19937().autoSeed(0)),
-    
+
     seed: function (kernel)
     {
         fuzzer.random = new Random(Random.engines.mt19937(),seed(kernel));
@@ -30,35 +38,35 @@ var fuzzer = {
         var file = fs.readFileSync(filePath,'utf-8');
         var lines = file.split('\n');
         var tempFilePath = filePath + 'test';
-        
+
         if(fs.existsSync(tempFilePath)) {
             fs.unlinkSync(tempFilePath);
         }
         lines.forEach( function (line) {
-            
+
             // Change the content of the string
-            if(fuzzer.random().bool(0.3) && line.match('\"(\\"|[^\"])*\"') && !line.match("//") && !line.match("@"))
-            line = line.replace(/\"(\\"|[^\"])*\"/g, '"' + fuzzer.random().string(10) + '"')
-            
+            if(fuzzer.random.bool(0.5) && line.match('\"(\\"|[^\"])*\"') && !line.match("//") && !line.match("@"))
+            line = line.replace(/\"(\\"|[^\"])*\"/g, '"' + fuzzer.random.string(10) + '"')
+
             //Replace '<' with '>' and '==' with '!=' and vice versa
             if( line.match("if") || line.match("while"))
             {
-                if(fuzzer.random().bool(0.25) && line.match("=="))
+                if(fuzzer.random.bool(0.4) && line.match("=="))
                     line = line.replace('==','!=');
-                if(fuzzer.random().bool(0.25) && line.match("!="))
+                if(fuzzer.random.bool(0.4) && line.match("!="))
                     line = line.replace('!=','==');
-                if(fuzzer.random().bool(0.25) && line.match("<"))
+                if(fuzzer.random.bool(0.4) && line.match("<"))
                     line = line.replace('<','>');
-                if(fuzzer.random().bool(0.25) && line.match(">"))
+                if(fuzzer.random.bool(0.4) && line.match(">"))
                     line = line.replace('>','<');
             }
 
             //Replace 0 with 1
-            if(fuzzer.random().bool(0.3) && line.match('"((\\"|[^"])+0(\\"|[^"])*|(\\"|[^"])*0(\\"|[^"])+)"'))
+            if(fuzzer.random.bool(0.3) && line.match('"((\\"|[^"])+0(\\"|[^"])*|(\\"|[^"])*0(\\"|[^"])+)"'))
             line = line.replace('0','1');
 
             // Replace 1 with 0
-            if(fuzzer.random().bool(0.3) && line.match('"((\\"|[^"])+1(\\"|[^"])*|(\\"|[^"])*1(\\"|[^"])+)"'))
+            if(fuzzer.random.bool(0.3) && line.match('"((\\"|[^"])+1(\\"|[^"])*|(\\"|[^"])*1(\\"|[^"])+)"'))
             line = line.replace('1','0');
 
             fs.appendFileSync(tempFilePath, line + '\n');
@@ -110,6 +118,7 @@ var fuzz = function (iterations)
         commitChanges(iterations);
 
     }
+    execSync(`cd /iTrust2-v2 && git checkout ${fuzzSHA}`);
 }
 
 fuzz(10)
